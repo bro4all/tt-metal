e1d20f4ffce5fc08304de82558811bb5c774a708
Nathan Sidwell <nsidwell@tenstorrent.com>
2025-07-29 17:37:37 +0000

    sfpi build 253

--- tt_metal/sfpi-version.sh
+++ tt_metal/sfpi-version.sh
@@ -1,13 +1,10 @@
 # set SFPI release version information
 
-sfpi_version=v6.13.0
+sfpi_version=v6.13.0-renumber
 sfpi_url=https://github.com/tenstorrent/sfpi/releases/download
 
 # convert md5 file into these variables
 # sed 's/^\([0-9a-f]*\) \*sfpi-\([a-z0-9_A-Z]*\)\.\([a-z]*\)$/sfpi_\2_\3_md5=\1/'
-sfpi_aarch64_Linux_txz_md5=6f680d44ba3155860b6acbafdd068397
-sfpi_aarch64_Linux_deb_md5=cacca03723888fb3ddfa9d9e1f879fb6
-sfpi_aarch64_Linux_rpm_md5=84aeeca966d608a561b0cbbc11430b41
-sfpi_x86_64_Linux_txz_md5=e5ee5a36037a0199e67141ae01003123
-sfpi_x86_64_Linux_deb_md5=0d282c8ca6c0403a9b6da4f7aedb16d6
-sfpi_x86_64_Linux_rpm_md5=9878daa5c0159e5e27e7910b947fa7e7
+sfpi_x86_64_Linux_txz_md5=f5ce7af5a8e7e11da55447123911e094
+sfpi_x86_64_Linux_deb_md5=33f48fbfb463783def9b8885ce80810a
+sfpi_x86_64_Linux_rpm_md5=b6cf8e41793b18cb275a72493e72ffb2
