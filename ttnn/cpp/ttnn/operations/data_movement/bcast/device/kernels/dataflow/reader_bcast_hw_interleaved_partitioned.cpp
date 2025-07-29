4c7a367363320570d79cacec6d555f8e44ca1110
Stanislav Minakov <sminakov@tenstorrent.com>
2025-07-29 01:47:12 -0700

    bcast

--- ttnn/cpp/ttnn/operations/data_movement/bcast/device/kernels/dataflow/reader_bcast_hw_interleaved_partitioned.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/bcast/device/kernels/dataflow/reader_bcast_hw_interleaved_partitioned.cpp
@@ -14,12 +14,6 @@ void kernel_main() {
     uint32_t curr_id_from_base = get_arg_val<uint32_t>(5);
     uint32_t bcast_id = get_arg_val<uint32_t>(6);
 
-#ifndef IN0_SHARDED
-    constexpr bool src0_is_dram = get_compile_time_arg_val(0) == 1;
-#endif
-
-    constexpr bool src1_is_dram = get_compile_time_arg_val(1) == 1;
-
     constexpr uint32_t cb_id_in0 = 0;
     constexpr uint32_t cb_id_in1 = 1;
     constexpr uint32_t onetile = 1;
@@ -34,15 +28,18 @@ void kernel_main() {
     uint32_t l1_write_addr_in1;
 
 #ifndef IN0_SHARDED
-    const InterleavedAddrGenFast<src0_is_dram> s0 = {
-        .bank_base_address = src0_addr, .page_size = in0_tile_bytes, .data_format = in0_data_format};
+    // When not sharded: src0 args at offset 0, src1 args at next offset
+    constexpr auto src0_tensor_args = TensorAccessorArgs<0>();
+    constexpr auto src1_tensor_args = TensorAccessorArgs<src0_tensor_args.next_compile_time_args_offset()>();
+    const auto s0 = TensorAccessor(src0_tensor_args, src0_addr, in0_tile_bytes);
+    const auto s1 = TensorAccessor(src1_tensor_args, src1_addr, in1_tile_bytes);
 #else
+    // When sharded: only src1 args are passed, at offset 0
+    constexpr auto src1_tensor_args = TensorAccessorArgs<0>();
+    const auto s1 = TensorAccessor(src1_tensor_args, src1_addr, in1_tile_bytes);
     cb_reserve_back(cb_id_in0, num_tiles);
     cb_push_back(cb_id_in0, num_tiles);
 #endif
-
-    const InterleavedAddrGenFast<src1_is_dram> s1 = {
-        .bank_base_address = src1_addr, .page_size = in1_tile_bytes, .data_format = in1_data_format};
 
 #ifdef BCAST_SCALAR
     cb_reserve_back(cb_id_in1, onetile);
