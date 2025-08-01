907fbe37af43114d334343d1273bac4e9efc1714
Bezulj Marko <mbezulj@tenstorrent.com>
2025-08-01 15:02:46 +0000

    work in progress

--- tests/ttnn/unit_tests/operations/reduce/test_cumsum.py
+++ tests/ttnn/unit_tests/operations/reduce/test_cumsum.py
@@ -75,12 +75,12 @@ def is_supported(shape, dim, ttnn_dtype):
         # ([64, 64, 1], 1),
         # ([1, 32, 129], 1),
         # ([33, 35, 37], 1),
-        ([1, 12, 40, 256], 0),
         ([1, 12, 40, 256], 1),
-        ([1, 24, 80, 256], 0),
+        ([1, 12, 40, 256], 2),
         ([1, 24, 80, 256], 1),
-        ([1, 48, 160, 256], 0),
+        ([1, 24, 80, 256], 2),
         ([1, 48, 160, 256], 1),
+        ([1, 48, 160, 256], 2),
         # ([7, 13, 129, 33], 1),
         # ([7, 13, 129, 33], 0),
         # ([4, 6, 128, 128], 0),
@@ -120,8 +120,10 @@ def test_cumsum(size, dim, dtypes, device):
     # which are not handled yet
     # torch_input_tensor = torch.randint(-2, 3, size=size, dtype=torch_dtype)
     torch_input_tensor = torch.rand(size=size, dtype=torch_dtype)
-    # input_tensor = ttnn.from_torch(torch_input_tensor, device=device, layout=ttnn.Layout.TILE, memory_config=ttnn.L1_MEMORY_CONFIG)
-    input_tensor = ttnn.from_torch(torch_input_tensor, device=device, layout=ttnn.Layout.TILE)
+    input_tensor = ttnn.from_torch(
+        torch_input_tensor, device=device, layout=ttnn.Layout.TILE, memory_config=ttnn.L1_MEMORY_CONFIG
+    )
+    # input_tensor = ttnn.from_torch(torch_input_tensor, device=device, layout=ttnn.Layout.TILE)
 
     expected_output_dtype = ttnn_dtype if ttnn_dtype is not None else input_tensor.dtype
 
@@ -137,6 +139,72 @@ def test_cumsum(size, dim, dtypes, device):
     torch_output = ttnn.to_torch(output_tensor, dtype=torch_dtype)
 
     expected_output = torch.cumsum(torch_input_tensor, dim=dim, dtype=torch_dtype)
+
+    comp_allclose_and_pcc(expected_output, torch_output)
+    if torch_output.numel() > 0:
+        assert_allclose(expected_output, torch_output, atol=4.0, rtol=1e-3)
+
+
+@pytest.mark.parametrize(
+    "size",
+    [
+        ([1, 12, 40, 256]),
+        ([1, 24, 80, 256]),
+        ([1, 48, 160, 256]),
+    ],
+)
+@pytest.mark.parametrize(
+    "dtypes",
+    [
+        (torch.bfloat16, ttnn.bfloat16),
+    ],
+)
+@pytest.mark.parametrize(
+    "memory_config",
+    [
+        ttnn.L1_MEMORY_CONFIG,
+        # ttnn.DRAM_MEMORY_CONFIG,
+    ],
+)
+def test_integral_image(size, dtypes, memory_config, device):
+    torch.manual_seed(29112024)
+
+    def integral_image(features):
+        return torch.cumsum(torch.cumsum(features, dim=-1), dim=-2)
+
+    def integral_image_channel_last(features):
+        assert len(features.shape) == 4, "Input tensor must be 4D"
+        assert features.shape[0] == 1, "Batch size must be 1"
+        return ttnn.cumsum(ttnn.cumsum(features, dim=1, dtype=features.dtype), dim=2, dtype=features.dtype)
+
+    (torch_dtype, ttnn_dtype) = dtypes
+
+    # Generate integer input on [-2; 2];
+    # by generating around 0, this avoids FP-related issues when adding large sums with small inputs
+    # which are not handled yet
+    # torch_input_tensor = torch.randint(-2, 3, size=size, dtype=torch_dtype)
+    torch_input_tensor = torch.rand(size=size, dtype=torch_dtype)
+    # input_tensor = ttnn.from_torch(torch_input_tensor, device=device, layout=ttnn.Layout.TILE, memory_config=ttnn.L1_MEMORY_CONFIG)
+    input_tensor = ttnn.from_torch(
+        torch_input_tensor, device=device, layout=ttnn.Layout.TILE, memory_config=memory_config
+    )
+
+    expected_output_dtype = ttnn_dtype if ttnn_dtype is not None else input_tensor.dtype
+
+    # For now, int32 version only supports >3-D tensors and `dim` outher than x and y axes
+    # if not is_supported(size, dim, expected_output_dtype):
+    #     pytest.skip("Unsupported configuration by ttnn.cumsum")
+
+    output_tensor = integral_image_channel_last(input_tensor)
+
+    assert output_tensor.dtype == expected_output_dtype
+    assert output_tensor.shape == (size)
+
+    torch_output = ttnn.to_torch(output_tensor, dtype=torch_dtype)
+
+    torch_input_channel_first = torch.permute(torch_input_tensor, (0, 3, 1, 2))
+    expected_output = integral_image(torch_input_channel_first)
+    expected_output = torch.permute(expected_output, (0, 2, 3, 1))
 
     comp_allclose_and_pcc(expected_output, torch_output)
     if torch_output.numel() > 0:
