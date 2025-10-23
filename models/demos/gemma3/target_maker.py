277f9d91bf6944cdde334e2f4d6e1bb78c2c0ddb
pmilojevicTT <pmilojevic@tenstorrent.com>
2025-10-23 12:19:55 +0000

    Added target maker script

--- models/demos/gemma3/target_maker.py
+++ models/demos/gemma3/target_maker.py
@@ -1,4 +1,8 @@
+import json
+from collections import defaultdict
+
 import pandas as pd
+from loguru import logger
 from tracy.process_model_log import get_latest_ops_log_filename
 
 from models.demos.llama3_70b_galaxy.tests.test_prefill_device_perf import (
@@ -11,6 +15,27 @@ from models.demos.llama3_70b_galaxy.tests.test_prefill_device_perf import (
 )
 from models.perf.benchmarking_utils import BenchmarkData, BenchmarkProfiler
 from models.perf.device_perf_utils import run_device_perf
+
+
+def chose_margin_based_on_variance(variance_percentage):
+    if variance_percentage < 1.0:
+        margin = 0.05  # 5% margin - very stable performance
+
+    elif variance_percentage < 2.0:
+        margin = 0.07  # 7% margin
+
+    elif variance_percentage < 3.0:
+        margin = 0.10  # 10% margin
+
+    elif variance_percentage < 5.0:
+        margin = 0.15  # 15% margin
+
+    elif variance_percentage < 10.0:
+        margin = 0.20  # 20% margin
+
+    else:
+        margin = 0.30  # 30% margin - high variance/unstable
+    return margin
 
 
 def compare_with_target(kernel_duration_per_instance_averaged_dict, perf_targets, profiler):
@@ -38,14 +63,6 @@ def compare_with_target(kernel_duration_per_instance_averaged_dict, perf_targets
                 op_name + "-model-kernel-min",
                 min_kernel_duration,
             )
-            benchmark_data.add_measurement(
-                profiler,
-                0,
-                step_name,
-                op_name + "-model-op_to_op-min",
-                dispatch_duration_per_instance_min_dict[op_code_with_id],
-            )
-
             # max
             benchmark_data.add_measurement(
                 profiler,
@@ -53,13 +70,6 @@ def compare_with_target(kernel_duration_per_instance_averaged_dict, perf_targets
                 step_name,
                 op_name + "-model-kernel-max",
                 max_kernel_duration,
-            )
-            benchmark_data.add_measurement(
-                profiler,
-                0,
-                step_name,
-                op_name + "-model-op_to_op-max",
-                dispatch_duration_per_instance_max_dict[op_code_with_id],
             )
 
             # Verify kernel duration is within tolerance
@@ -115,8 +125,8 @@ def test():
     all_results_max = []
     all_results_min = []
 
-    # Run tracy 3 times and make target out of it
-    for i in range(3):
+    # Run tracy 7 times and make target out of it
+    for i in range(7):
         post_processed_results = run_device_perf(command, subdir, num_iterations, cols, batch_size, has_signposts=False)
         profiler.end("PROFILLING OP TO OP")
         profiler.end("run")
@@ -140,7 +150,66 @@ def test():
         all_results_max.append(kernel_duration_per_instance_max_dict)
         all_results_min.append(kernel_duration_per_instance_min_dict)
 
+        logger.info(f"Iteration {i}/7 done.")
+
     logger.info(f"Generated target kernel durations: {all_results_average}")
+
+    # Write to file
+    with open("performance_results_najnoviji_avg.json", "w") as f:
+        json.dump(all_results_average, f, indent=2)
+
+    # Write to file
+    with open("performance_results_najnoviji_min.json", "w") as f:
+        json.dump(all_results_min, f, indent=2)
+
+    # Write to file
+    with open("performance_results_najnoviji_max.json", "w") as f:
+        json.dump(all_results_max, f, indent=2)
+
+    with open("performance_results_najnoviji_avg.json", "r") as f:
+        data_avg = json.load(f)
+
+    max_values = defaultdict(float)
+    min_values = defaultdict(float)
+    avg_values = defaultdict(float)
+
+    for record in data_avg:
+        for op in record.keys():
+            if op not in avg_values:
+                avg_values[op] = 0.0
+                max_values[op] = record[op]
+                min_values[op] = record[op]
+            max_values[op] = max(max_values[op], record[op])
+            min_values[op] = min(min_values[op], record[op])
+            avg_values[op] += record[op]
+
+    length = float(len(data_avg))
+    for op in avg_values.keys():
+        avg_values[op] /= length
+
+    logger.info(f"Generated average values: {avg_values}")
+    logger.info(f"Generated min values: {min_values}")
+    logger.info(f"Generated max values: {max_values}")
+
+    variance_values_percentages = defaultdict(float)
+    for op in avg_values.keys():
+        variance = (max_values[op] - min_values[op]) / avg_values[op]
+        variance_values_percentages[op] = variance * 100.00
+
+    logger.info(f"Generated variance values: {variance_values_percentages}")
+
+    with open("performance_results_variance.json", "w") as f:
+        json.dump(variance_values_percentages, f, indent=2)
+
+    margin_values = defaultdict(float)
+    for op in variance_values_percentages.keys():
+        margin = chose_margin_based_on_variance(variance_values_percentages[op])
+        margin_values[op] = margin
+
+    logger.info(f"Generated margin values: {margin_values}")
+
+    with open("performance_results_margins.json", "w") as f:
+        json.dump(margin_values, f, indent=2)
 
     # expected_perf_cols = {}
     # with open(f"models/demos/gemma3/tests/perf_targets/targets_test_perf_vision_cross_attention_ops.json", "r") as f:
